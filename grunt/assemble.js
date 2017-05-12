318fcb7e20e15d14a3ba3a72916781d80e1be562
Matt Bain <mattb@solegraphics.com>
2017-05-11 21:40:17 -0700

    feat: strip extensions from asseble parsing. run juice on all file types.

--- grunt/assemble.js
+++ grunt/assemble.js
@@ -5,7 +5,8 @@ module.exports = {
     partials: ['<%= paths.src %>/partials/**/*.hbs'],
     helpers: ['<%= paths.src %>/helpers/**/*.js'],
     data: ['<%= paths.src %>/data/*.{json,yml}'],
-    flatten: true
+    flatten: true,
+    ext: '',
   },
   pages: {
     src: ['<%= paths.src %>/emails/*.hbs'],
